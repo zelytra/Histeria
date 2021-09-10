9d879edc1fd724b1a029f699136fb202fd731694
Zelytra <alexbreuillet@gmail.com>
2021-09-10 14:27:11 +0200

    Near command

--- src/main/java/fr/zelytra/histeria/Histeria.java
+++ src/main/java/fr/zelytra/histeria/Histeria.java
@@ -13,6 +13,7 @@ import fr.zelytra.histeria.commands.hguard.HGuardTabCompleter;
 import fr.zelytra.histeria.commands.lang.LangCommand;
 import fr.zelytra.histeria.commands.lang.LangTabCommand;
 import fr.zelytra.histeria.commands.miscellaneous.*;
+import fr.zelytra.histeria.commands.near.Near;
 import fr.zelytra.histeria.commands.serverSwitch.ServerSelector;
 import fr.zelytra.histeria.commands.wiki.Wiki;
 import fr.zelytra.histeria.events.EventManager;
@@ -131,6 +132,7 @@ public final class Histeria extends JavaPlugin {
         getCommand("bug").setExecutor(new Bug());
         getCommand("social").setExecutor(new Social());
         getCommand("compress").setExecutor(new Compress());
+        getCommand("near").setExecutor(new Near());
 
 
     }
---
+++ src/main/java/fr/zelytra/histeria/commands/near/Near.java
@@ -0,0 +1,41 @@
+package fr.zelytra.histeria.commands.near;
+
+import fr.zelytra.histeria.managers.player.CustomPlayer;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Near implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        if (!(sender instanceof Player)) return false;
+
+        Player player = (Player) sender;
+        List<String> playerList = new ArrayList();
+        DecimalFormat dec = new DecimalFormat("#0.00");
+
+        for (Entity entity : player.getNearbyEntities(50, 50, 50)) {
+            if (entity instanceof Player) {
+                playerList.add("§b- " + entity.getName() + " §6["
+                        + dec.format(
+                        (Math.sqrt(Math.pow(entity.getLocation().getX() - player.getLocation().getX(), 2)
+                                + Math.pow(entity.getLocation().getZ() - player.getLocation().getZ(), 2))))
+                        + "bl]");
+            }
+        }
+        player.sendMessage("§9|--------[§b"+ CustomPlayer.getCustomPlayer(player.getName()).getLang().get("command.nearTitle") +"§9]--------|");
+
+        for (String string : playerList) {
+            player.sendMessage(string);
+        }
+        return true;
+    }
+}
