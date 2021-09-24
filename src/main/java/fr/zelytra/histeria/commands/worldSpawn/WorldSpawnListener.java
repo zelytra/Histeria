c2c68e4a4c7b48e925b78d061a457714f38038f2
Zelytra <alexbreuillet@gmail.com>
2021-09-24 16:49:47 +0200

    Fix death world respawn

--- src/main/java/fr/zelytra/histeria/commands/worldSpawn/WorldSpawnListener.java
+++ src/main/java/fr/zelytra/histeria/commands/worldSpawn/WorldSpawnListener.java
@@ -8,11 +8,18 @@ package fr.zelytra.histeria.commands.worldSpawn;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
 
 public class WorldSpawnListener implements Listener {
 
     @EventHandler
     public void onJoin(PlayerJoinEvent e) {
         e.getPlayer().teleport(WorldSpawn.worldSpawn.getSpawn());
+    }
+
+    @EventHandler
+    public void onRespawn (PlayerRespawnEvent e){
+        System.out.println("trigger");
+        e.setRespawnLocation(WorldSpawn.worldSpawn.getSpawn());
     }
 }
