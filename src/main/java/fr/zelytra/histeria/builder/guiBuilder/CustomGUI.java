caee39dd386fce293ac777ef63f06091495ed286
Zelytra <alexbreuillet@gmail.com>
2021-09-28 15:44:59 +0200

    Edit copyright

--- src/main/java/fr/zelytra/histeria/builder/guiBuilder/CustomGUI.java
+++ src/main/java/fr/zelytra/histeria/builder/guiBuilder/CustomGUI.java
@@ -1,3 +1,12 @@
+/*
+ * Copyright (c) 2021.
+ * Made by Zelytra :
+ *  - Website : https://zelytra.fr
+ *  - GitHub : http://github.zelytra.fr
+ *
+ * All right reserved
+ */
+
 package fr.zelytra.histeria.builder.guiBuilder;
 
 import org.bukkit.inventory.Inventory;
