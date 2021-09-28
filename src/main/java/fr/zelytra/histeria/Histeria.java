f7a6353ccb7454dc92392ef611c79a059318cec3
Zelytra <alexbreuillet@gmail.com>
2021-09-28 15:00:34 +0200

    another master test branch

--- src/main/java/fr/zelytra/histeria/Histeria.java
+++ src/main/java/fr/zelytra/histeria/Histeria.java
@@ -53,7 +53,7 @@ public final class Histeria extends JavaPlugin {
 
     private static Histeria instance;
     public static boolean log = true;
-    public static boolean synchro = false;
+    public static boolean synchro = true;
     private static boolean saberFaction = false;
     private static LuckPerms luckPerms;
 
@@ -100,6 +100,7 @@ public final class Histeria extends JavaPlugin {
     public void onDisable() {
         configurationManager.unload();
         //TODO Save all data of players in case of shutting down server
+        //another test
 
     }
 
