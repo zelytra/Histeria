f256444764f43d25566838e44b0986bfb2aed814
Zelytra <alexbreuillet@gmail.com>
2021-12-08 11:44:49 +0100

    Logs ancien debris

--- src/main/java/fr/zelytra/histeria/managers/logs/listener/OreLogger.java
+++ src/main/java/fr/zelytra/histeria/managers/logs/listener/OreLogger.java
@@ -21,10 +21,13 @@ public class OreLogger implements Listener {
     public void onOre(BlockBreakEvent e) {
         switch (e.getBlock().getType()) {
             case DIAMOND_ORE:
-                new DiscordLog(WebHookType.ORE, e.getPlayer().getName() + " a mine un minerais de **DIAMAND** ");
+                new DiscordLog(WebHookType.ORE, e.getPlayer().getName() + " a miné un minerais de **DIAMAND** ");
                 break;
             case CHISELED_NETHER_BRICKS:
-                new DiscordLog(WebHookType.ORE, e.getPlayer().getName() + " a mine un minerais d' **HISTERITE** ");
+                new DiscordLog(WebHookType.ORE, e.getPlayer().getName() + " a miné un minerais d' **HISTERITE** ");
+                break;
+            case ANCIENT_DEBRIS:
+                new DiscordLog(WebHookType.ORE, e.getPlayer().getName() + " a miné un minerais d' **ANCIEN DEBRIS** ");
                 break;
         }
     }
