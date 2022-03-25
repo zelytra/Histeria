9cbfdf6f71feba03764b3a086e18ccd24209ddb2
Zelytra <alexbreuillet@gmail.com>
2022-03-25 11:47:57 +0100

    Update lang files

---
+++ src/main/java/fr/zelytra/histeria/managers/jobs/command/AdminJob.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2022.
+ * Made by Zelytra :
+ *  - Website : https://zelytra.fr
+ *  - GitHub : http://github.zelytra.fr
+ *
+ * All right reserved
+ */
+
+package fr.zelytra.histeria.managers.jobs.command;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.jetbrains.annotations.NotNull;
+
+public class AdminJob implements CommandExecutor {
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        return false;
+    }
+}
