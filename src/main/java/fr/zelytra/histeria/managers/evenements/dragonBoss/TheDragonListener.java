8ed6865ae77969e27b0d97c0bc201ccc30736461
Zelytra <apetit@et.esiea.fr>
2022-03-31 16:00:56 +0200

    Adding crystal repulsion

--- src/main/java/fr/zelytra/histeria/managers/evenements/dragonBoss/TheDragonListener.java
+++ src/main/java/fr/zelytra/histeria/managers/evenements/dragonBoss/TheDragonListener.java
@@ -9,13 +9,19 @@
 
 package fr.zelytra.histeria.managers.evenements.dragonBoss;
 
+import org.bukkit.Particle;
 import org.bukkit.entity.AbstractArrow;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.util.Vector;
+
+import java.util.List;
 
 public class TheDragonListener implements Listener {
 
@@ -40,6 +46,30 @@ public class TheDragonListener implements Listener {
             }
         }
 
+    }
+
+    @EventHandler
+    public void onCrystal(EntityDamageByEntityEvent e) {
+
+        // Player ejected when destroying ender cystal
+        if (e.getEntityType() == EntityType.ENDER_CRYSTAL && e.getEntity().getWorld().getName().equalsIgnoreCase("world_the_end")) {
+            List<Entity> nearEntities = e.getEntity().getNearbyEntities(10, 10, 10);
+            for (Entity entity : nearEntities) {
+
+                Vector delta = new Vector(entity.getLocation().getX() - e.getEntity().getLocation().getX(), 0, entity.getLocation().getZ() - e.getEntity().getLocation().getZ());
+                double norme = Math.sqrt(Math.pow(delta.getX(), 2) + Math.pow(delta.getY(), 2) + Math.pow(delta.getZ(), 2));
+                double coef = 1.8;
+                Vector dir = new Vector((delta.getX() / norme) * coef, (delta.getY() / norme) + 1.5, (delta.getZ() / norme) * coef);
+
+                try {
+                    entity.setVelocity(dir);
+                } catch (Exception ignored) {
+
+                }
+
+            }
+            e.getEntity().getWorld().spawnParticle(Particle.DRAGON_BREATH, e.getEntity().getLocation(), 190);
+        }
     }
 
     @EventHandler
